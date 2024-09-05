9c0def260ef4d926ffd2bd67211646117b3fe325
Maxi Ferreira <charca@gmail.com>
2024-09-04 21:49:27 -0700

    Using the restaurant model

--- apps/web/modules/restaurant/features/restaurant-header/components/restaurant-information.tsx
+++ apps/web/modules/restaurant/features/restaurant-header/components/restaurant-information.tsx
@@ -1,5 +1,16 @@
-export const RestaurantInformation = () => (
-  <div className="bg-green-100 border-green-600 text-green-600 border-2 p-4 rounded-lg flex items-center justify-center text-2xl font-mono">
+import { Restaurant } from '../../../models/types'
+
+export const RestaurantInformation = ({
+  restaurant,
+}: {
+  restaurant: Restaurant
+}) => (
+  <div className="bg-green-100 border-green-600 text-green-600 border-2 p-4 rounded-lg flex flex-col items-center justify-center text-2xl font-mono">
     <h1>RestaurantInformation</h1>
+    <ul className="flex flex-col gap-2 mt-4 text-base w-full">
+      <li>Name: {restaurant.name}</li>
+      <li>Logo URL: {restaurant.logoUrl}</li>
+      <li>Rating: {restaurant.rating}</li>
+    </ul>
   </div>
 )
--- apps/web/modules/restaurant/features/restaurant-header/restaurant-header.tsx
+++ apps/web/modules/restaurant/features/restaurant-header/restaurant-header.tsx
@@ -1,12 +1,15 @@
+import { getRestaurant } from '../../models/restaurant'
 import { MenuSearchBar } from './components/menu-search-bar'
 import { RestaurantInformation } from './components/restaurant-information'
 
 export async function RestaurantHeader() {
+  const restaurant = await getRestaurant('1')
+
   return (
     <div className="bg-purple-100 border-purple-600 border-2 p-4 rounded-lg flex flex-col items-center justify-center text-2xl font-mono text-purple-600">
       <h1 className="p-4">RestaurantHeader</h1>
       <div className="flex flex-row w-full justify-between">
-        <RestaurantInformation />
+        <RestaurantInformation restaurant={restaurant} />
         <MenuSearchBar />
       </div>
     </div>
